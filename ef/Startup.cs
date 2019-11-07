b719b6bd3071aa25fb6c7ebe44d14e5634226a79
xtidessgeq <1046909021@QQ.com>
2019-11-07 13:45:55 +0800

    d

--- ef/Startup.cs
+++ ef/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ef
+namespace ef 
 {
     public class Startup
     {
